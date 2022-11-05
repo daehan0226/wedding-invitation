7007649ce2e37cb5024c8f288a8e2027793c1726
daehan <dh16931@gmail.com>
2022-11-05 21:34:53 +0900

    feat: travel info

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,3 @@
 export const MAIL='Alex.dycus23@gmail.com'
 export const API='43.201.44.78:16790'
+export const GOOGLEMapApiKey='AIzaSyCpmHv9f2_7W6kKpYqfa5ZFG0cEzvo8mC4'
--- client/src/pages/Travel.js
+++ client/src/pages/Travel.js
@@ -1,42 +1,66 @@
+import React from 'react'
 import Box from '@mui/material/Box';
 import GoogleMapReact from 'google-map-react';
-
+import { GOOGLEMapApiKey } from '../config/config'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Typography from '@mui/material/Typography';
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
 
 const defaultProps = {
   center: {
-    lat: 38.64979946281438,
-    lng: -90.26612840337923
+    lat: 37.66377,
+    lng: -89.22364
   },
-  zoom: 11
+  zoom: 13
 };
 
-const googleMapApiKey = "AIzaSyCpmHv9f2_7W6kKpYqfa5ZFG0cEzvo8mC4"
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = () => <LocationOnIcon />;
 
 function Travel() {
   return (
     <Box m={2} sx={{
       display: 'flex',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+      flexDirection: 'column'
     }}>
-      <Box style={{ 
+      <Box mb={2} style={{ 
         height: '50vh', 
         width: '100%',
         maxWidth: '800px'
       }}>
+        <Typography mb={{ mobile: 4, laptop: 6}} variant="h4" color={'text.white'} sx={{textAlign: 'center'}} >
+          The Wedding Barn at Black Jack Vineyard<a href="https://www.blackjackvineyards.com/" target="_blank" ><InsertLinkIcon color="info" /></a>
+        </Typography>
+        <Box>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/venue.jpg`}
+            width='100%'
+            height='auto'
+            alt=''
+            style={{
+              maxWidth: 600,
+              minWidth: 300
+            }}
+          />
+        <FacebookIcon />
+
+        <AirplanemodeActiveIcon />
+      </Box>
+      <Box m={3}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: googleMapApiKey }}
+          bootstrapURLKeys={{ key: GOOGLEMapApiKey }}  // GOOGLEMapApiKey
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         >
           <AnyReactComponent
             lat={defaultProps.center.lat}
             lng={defaultProps.center.lng}
-            text="Venue"
           />
         </GoogleMapReact>
+      </Box>
       </Box>  
     </Box>
   );
