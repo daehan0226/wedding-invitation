170eb3474847101009a37217466beb4b893912c7
daehan <dh16931@gmail.com>
2023-01-29 16:54:57 +0900

    feat: login on manage page

--- client/src/pages/manage/Manage.js
+++ client/src/pages/manage/Manage.js
@@ -2,15 +2,33 @@ import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import ManageRsvp from './ManageRsvp';
 import { getRsvp } from '../../helper/api';
+import LoginComponent from './LoginComponent';
+import jwt_decode from "jwt-decode";
 
 function Manage() {
+  const [ loggedIn, setLoggedIn ] = useState(false)
+  const [ userId, setUserId ] = useState("")
   const [ rsvp, setRsvp ] = useState([])
   useEffect(()=>{
     async function fetchData() {
       const data = await getRsvp()
       setRsvp([...data])
     }
-    fetchData();
+    if (loggedIn) {
+      fetchData();
+    }
+  }, [loggedIn])
+
+  useEffect(()=>{
+    setLoggedIn(false)
+    try {
+      const token = localStorage.getItem('accessToken');
+      const decoded = jwt_decode(token);
+      if (decoded?.sub) {
+        setUserId(decoded.sub)
+        setLoggedIn(true)
+      }
+    } catch {}
   }, [])
 
   return (
@@ -21,7 +39,11 @@ function Manage() {
         position: 'relative',
       }}
     >
-      <ManageRsvp  rsvpList={rsvp} />
+      {loggedIn ? (
+        <ManageRsvp  rsvpList={rsvp} />
+      ) : (
+        <LoginComponent />
+      )}
     </Box>
   );
 }
